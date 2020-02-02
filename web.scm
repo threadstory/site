0027e95360e035459d6c2fe3108dd200ec222101
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2020-02-02 21:21:55 +0530

    add popular products

--- web.scm
+++ web.scm
@@ -135,6 +135,128 @@
 		  (img (@ (class "w-full md:w-4/5 z-50")
 			  (src "pictures/hero.png")))))))
 
+(define wavy-svg
+  `(div (@ (class "relative -mt-12 lg:-mt-24"))
+	(svg (@ (viewBox "0 0 1428 174")
+		(version "1.1")
+		(xmlns "http://www.w3.org/2000/svg")
+		(xmlns:xlink "http://www.w3.org/1999/xlink"))
+	     (g (@ (stroke "none")
+		   (stroke-width "1")
+		   (fill "none")
+		   (fill-rule "evenodd"))
+		(g (@ (transform "translate(-2.000000, 44.000000)")
+		      (fill "#ffffff")
+		      (fill-rule "nonzero"))
+		   (path (@ (d "M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496")
+			    (opacity "0.100000001")))
+
+		   (path (@ (d "M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z")
+			    (opacity "0.100000001")))
+
+		   (path (@ (d "M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z")
+			    (id "Path-4")
+			    (opacity "0.200000003"))))
+		(g (@ (transform "translate(-4.000000, 76.000000)")
+		      (fill "#ffffff")
+		      (fill-rule "nonzero"))
+		   (path (@ (d "M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"))))))))
+
+
+(define section-underline
+  '(div (@ (class "w-full mb-4"))
+	(div (@ (class "h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t")))))
+
+(define section-title
+  (lambda (txt)
+    `((h1 (@ ,(classes '(w-full my-2 text-5xl font-bold leading-tight text-center
+				text-gray-800)))
+	  ,txt)
+      ,section-underline)))
+
+
+(define title
+  (lambda (txt)
+    `(h3 (@ (class "text-3xl text-gray-800 font-bold leading-none mb-3"))
+	 ,txt)))
+
+
+
+(define promise-section
+  `(section (@ (class "bg-white border-b py-8"))
+	    (div (@ (class "container max-w-5xl mx-auto m-8"))
+		 
+		 ,@(section-title "Our Promise")
+		 
+		 
+		 (div (@ (class "flex flex-wrap"))
+		      
+		      (div (@ (class "w-5/6 sm:w-1/2 p-6"))
+			   ,(title "Express your brand")
+			   (p (@ (class "text-gray-600 mb-8"))
+			      "Your brand should be on the finest of product."))
+
+		      (div (@ (class "w-full sm:w-1/2 p-6"))
+			   (img (@ (class "w-full sm:h-64 mx-auto")
+				   (src "pictures/brand.webp")))))
+
+		 (div (@ (class "flex flex-wrap flex-col-reverse sm:flex-row"))
+
+		      (div (@ (class "w-full sm:w-1/2 p-6 mt-6"))
+			   (img (@ (class "w-full sm:h-64 mx-auto")
+				   (src "pictures/brand.webp"))))
+		      
+		      (div (@ (class "w-full sm:w-1/2 p-6 mt-6"))
+			   (div (@ (class "align-middle"))
+				,(title "High quality materials"))
+			   (p (@ (class "text-gray-600 mb-8"))
+			      "Your brand should be on the finest of product."))))))
+
+(define-record-type card (fields name description image))
+
+
+(define (cards->sxml cards)
+  (define (card->sxml card)
+    `(div (@ ,(classes '(w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink)))
+	  (div (@ (class "max-w-sm rounded overflow-hidden shadow-lg"))
+	       (img (@ (class "w-full")
+		       (src ,(card-image card))
+		       (alt ,(card-name card))))
+	       (div (@ (class "px-6 py-4"))
+		    (div (@ (class "font-bold text-xl text-black mb-2"))
+			 ,(card-name card))
+		    (p (@ (class "text-gray-700 text-base"))
+		       ,(card-description card))))))
+  
+  `(div (@ (class "flex-wrap flex max-w-6xl mx-auto"))
+	,@(map card->sxml cards)))
+
+
+
+(define products (list (make-card "Hoodies"
+				  "Comfortable pullovers for your winters"
+				  "pictures/hoodie.webp")
+		       
+		       (make-card "Round neck t-shirts"
+				  "Comfortable cotton t-shirts"
+				  "pictures/tshirt.webp")
+
+		       (make-card "Caps"
+				  "Caps for summers"
+				  "pictures/caps.webp")
+
+		       ))
+
+(define popular-products
+  `(section (@ (class "bg-white border-p py-8"))
+
+	    (div (@ (class "container mx-auto flex flex-wrap pt-4 pb-12"))
+
+		 ,@(section-title "Popular Products")
+
+		 ,(cards->sxml products))))
+
+
 (define generate-index-page
   (lambda ()
     (with-output-to-file "dist/index.html"
@@ -145,7 +267,13 @@
 			       
 			       ,(navbar 'home '(home products about))
 
-			       ,hero))))
+			       ,hero
+
+			       ,wavy-svg
+
+			       ,promise-section
+
+			       ,popular-products))))
       'replace)))
 
 
